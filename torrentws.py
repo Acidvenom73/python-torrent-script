a9264e9f05388eb962a97de4547e49c3d5d66e9d
BuildTools <acidvenom@hotmail.com>
2020-02-11 00:03:48 +0100

    Removed broken parts

--- torrentws.py
+++ torrentws.py
@@ -8,15 +8,11 @@ from webbrowser import open
 def main():
 
     ans = ""
-    print("Options:\nPress 1 for Batch Download\nPress 2 for Single Download")
-    while(not ans == "1" and not ans == "2"):
+    print("Options: Press 1 for Single Download")
+    while(not ans == "1"):
         ans = input()
         if ans == "1":
-            batch()
-        elif ans == "2":
-            loop = single()
-            while(loop):
-                loop = single()
+            single()
         elif ans == "X" or ans == "C":
             break
     return
@@ -68,62 +64,62 @@ def single():
     print(link)
     return True
 
-def batch():
-    print("BATCH DOWNLOAD")
+# def batch():
+#     print("BATCH DOWNLOAD")
 
-    episode_counter = 0
-    print("Torrent name:")
-    search = input()
+#     episode_counter = 0
+#     print("Torrent name:")
+#     search = input()
 
-    print("Season: ", end="")
-    season = "S0" + input()
+#     print("Season: ", end="")
+#     season = "S0" + input()
 
-    while(True):
-        episode_counter+=1
-        if episode_counter < 10:
-            episode = "E0" + str(episode_counter)
-        else:
-            episode = str(episode_counter)
-        url = "https://kickasstorrents.to/usearch/" + search + " " + season + episode
-        print(url)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        try:
-            Data = list(soup.findAll(title="Download torrent file"))
-        except IndexError:
-            print("No more episodes found!")
-            return True
-        i = 0
-        while(True):
-            ans = ""
-            try:
-                link = str(Data[i])
-            except IndexError:
-                print("Out of torrents!")
-                return True
-            link = link.partition(" target=")[0]
-            link = link.partition("/download/")[2]
-            link = link.replace("\"", "")
-            print(link)
-            while(ans != "Y" and ans != "N" and ans != "C"):
-                print("Y = Yes, N = No, C = Cancel")
-                ans = input()
-            if ans == "Y":
-                break
-            elif ans == "C":
-                return False
-            i+=1
+#     while(True):
+#         episode_counter+=1
+#         if episode_counter < 10:
+#             episode = "E0" + str(episode_counter)
+#         else:
+#             episode = str(episode_counter)
+#         url = "https://kickasstorrents.to/usearch/" + search + " " + season + episode
+#         print(url)
+#         response = requests.get(url)
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         try:
+#             Data = list(soup.findAll(title="Download torrent file"))
+#         except IndexError:
+#             print("No more episodes found!")
+#             return True
+#         i = 0
+#         while(True):
+#             ans = ""
+#             try:
+#                 link = str(Data[i])
+#             except IndexError:
+#                 print("Out of torrents!")
+#                 return True
+#             link = link.partition(" target=")[0]
+#             link = link.partition("/download/")[2]
+#             link = link.replace("\"", "")
+#             print(link)
+#             while(ans != "Y" and ans != "N" and ans != "C"):
+#                 print("Y = Yes, N = No, C = Cancel")
+#                 ans = input()
+#             if ans == "Y":
+#                 break
+#             elif ans == "C":
+#                 return False
+#             i+=1
 
-        url = "https://kickasstorrents.to/" + link
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        link = str(soup.findAll(title="Magnet link"))
-        link = link.partition("title=")[0]
-        link = link.partition("href=")[2]
-        link = link.replace("\"", "")
+#         url = "https://kickasstorrents.to/" + link
+#         response = requests.get(url)
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         link = str(soup.findAll(title="Magnet link"))
+#         link = link.partition("title=")[0]
+#         link = link.partition("href=")[2]
+#         link = link.replace("\"", "")
 
-        open(link)
-        print(link)
+#         open(link)
+#         print(link)
 
 
 if __name__ == "__main__":
